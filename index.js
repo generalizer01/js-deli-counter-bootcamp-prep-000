ee2f18dfe4864c922185bc7568730a30762921b6
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-07 13:49:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function takeANumber(line,person) {
     return (`${person}, you are ${line.length} in the line`) 
 }
 
+function nowServing(line) {
+	if (line.length>0) {
+	    return line.shift()
+    }
+	else {
+		return ("There is nobody waiting to be served!") }
+}
+
+function 
